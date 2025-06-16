88dda08c9cd961e2aaa942b873bb70119bbffd35
MarcelloAnthony20 <anthonymarcello20@gmail.com>
2025-06-15 21:35:40 -0300

    Configurando Banco de Dados com as entidades.

--- app/src/main/java/com/example/atividade3/Database/AppDatabase.java
+++ app/src/main/java/com/example/atividade3/Database/AppDatabase.java
@@ -6,11 +6,18 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
+import com.example.atividade3.DAO.EventoDAO;
+import com.example.atividade3.DAO.UsuarioDAO;
+import com.example.atividade3.Entities.Evento;
+import com.example.atividade3.Entities.Usuario;
 
 
-@Database(entities = {}, version = 1)
+@Database(entities = {Evento.class, Usuario.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase{
+    public abstract UsuarioDAO usuarioDAO();
+    public abstract EventoDAO eventoDAO();
     private static AppDatabase db;
+
 
     public static AppDatabase getDatabase(Context context){
         if(db==null){
